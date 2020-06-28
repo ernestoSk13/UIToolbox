14249ac52b4c47a711f301c1c48561666f8e46fd
Ernesto Sánchez Kuri <ernesto.sanchez@oracle.com>
2020-06-27 19:28:26 -0500

    spm changes

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "UIToolbox",
+    platforms: [
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -20,11 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "UIToolbox",
-            dependencies: [],
-            path: "Sources"),
+            dependencies: []),
         .testTarget(
             name: "UIToolboxTests",
-            dependencies: ["UIToolbox"],
-            path: "Tests"),
+            dependencies: ["UIToolbox"]),
     ]
 )
